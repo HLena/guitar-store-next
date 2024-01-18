ca12de3857d6198fc0ece76a2428ca984d7e814f
HLena <gordillo.vi20@gmail.com>
2024-01-18 17:48:02 -0500

    Set the Layout component

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,14 @@
-import styles from '../styles/page.module.css'
+import { Layout } from '../components/layout'
 import Link from 'next/link'
 
 export default function Home() {
   return (
-    <>
+    <Layout
+      title={'Venta de guitarras'}
+      description={'Venta de guitarras, blog y musica'}
+    >
         <h1>Hola Mundo</h1>
         <Link href="nosotros">Nosotros</Link>
-    </>
+    </Layout>
   )
 }
--- pages/nosotros.js
+++ pages/nosotros.js
@@ -1,9 +1,13 @@
 import Link from 'next/link'
+import { Layout } from '../components/layout'
 export default function Nosotros()  {
   return (
-    <>
+    <Layout
+      title={'Nosotros'}
+      description={'Sobre nosotros, GuitarLs, tienda de mùsica'}
+    >
         <div>nosotros</div>
         <Link href="/">Inicio</Link>
-    </>
+    </Layout>
   )
 }
