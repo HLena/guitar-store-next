7ad0c17eb0ed90e1c1318115de7511a4d710f092
HLena <gordillo.vi20@gmail.com>
2024-01-18 19:11:10 -0500

    Add styles to header and links

--- components/header.jsx
+++ components/header.jsx
@@ -2,17 +2,26 @@ import Image from 'next/image'
 import Link from 'next/link'
 import logo from '../public/img/logo.svg'
 import styles from '../styles/header.module.css'
+import { useRouter } from 'next/router'
 
 export const Header = () => {
+
+  const router = useRouter();
   return (
     <header className={styles.header}>
       <div className={`contenedor ${styles.barra}`}>
-        <Image src={logo} alt="logo" width={300} height={40}/>
+        <Link href="/">
+          <Image src={logo} alt="logo" width={300} height={40}/>
+        </Link>
         <nav className={styles.navegacion}>
-          <Link href="/">Inicio</Link>
-          <Link href="/nosotros">Nosotros</Link>
-          <Link href="/blog">Blog</Link>
-          <Link href="/store">Tienda</Link>
+          <Link href="/" className={router.pathname === '/' ? styles.active : ''}> Inicio 
+              </Link>
+          <Link href="/nosotros" className={router.pathname === '/nosotros' ? styles.active : ''}> Nosotros 
+              </Link>
+          <Link href="/blog" className={router.pathname === '/blog' ? styles.active : ''}> Blog 
+              </Link>
+          <Link href="/store" className={router.pathname === '/store' ? styles.active : ''}> Tienda 
+              </Link>
         </nav>
       </div>
     </header>
