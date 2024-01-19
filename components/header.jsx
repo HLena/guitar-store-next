fa05843874c4ad16e12827a2b64c3df551523a04
HLena <gordillo.vi20@gmail.com>
2024-01-18 19:02:27 -0500

    Add navigation links

--- components/header.jsx
+++ components/header.jsx
@@ -1,11 +1,19 @@
 import Image from 'next/image'
+import Link from 'next/link'
 import logo from '../public/img/logo.svg'
+import styles from '../styles/header.module.css'
 
 export const Header = () => {
   return (
-    <header>
-      <div className="contenedor">
+    <header className={styles.header}>
+      <div className={`contenedor ${styles.barra}`}>
         <Image src={logo} alt="logo" width={300} height={40}/>
+        <nav className={styles.navegacion}>
+          <Link href="/">Inicio</Link>
+          <Link href="/nosotros">Nosotros</Link>
+          <Link href="/blog">Blog</Link>
+          <Link href="/store">Tienda</Link>
+        </nav>
       </div>
     </header>
   )
