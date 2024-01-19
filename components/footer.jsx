eaf5233a1d0f600621147f8436a26c9b4c24a652
HLena <gordillo.vi20@gmail.com>
2024-01-18 19:28:26 -0500

    create a component and set footer styles

--- components/footer.jsx
+++ components/footer.jsx
@@ -1,6 +1,27 @@
+import Link from "next/link";
+import styles from '../styles/footer.module.css'
 
 export const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className={styles.footer}>
+      <div className={`contenedor ${styles.contenido}`}>
+      <nav className={styles.navegacion}>
+          <Link 
+            href="/" className={''}> Inicio 
+          </Link>
+          <Link 
+            href="/nosotros" className="" > Nosotros 
+          </Link>
+          <Link 
+            href="/blog" className=""> Blog 
+          </Link>
+          <Link 
+            href="/store" className=""> Tienda 
+          </Link>
+        </nav>
+
+        <p className={styles.copyright}>Todos los derechos reservados { new Date().getFullYear()}</p>
+      </div>
+    </footer>
   )
 }
