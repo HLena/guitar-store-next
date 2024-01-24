a987d380e8c1476a46c4a541ad40f00daddacffd
HLena <gordillo.vi20@gmail.com>
2024-01-23 22:17:37 -0500

    Add getServerSideProps to get data

--- pages/store.js
+++ pages/store.js
@@ -1,14 +1,55 @@
+import { Guitar } from '../components/guitar'
 import { Layout } from '../components/layout'
-import Link from 'next/link'
+import styles from '../styles/guitarras.module.css'
 
-export default function Store() {
+export default function Store({guitars}) {
   return (
     <Layout
-      title={'Venta de guitarras'}
-      description={'Venta de guitarras, blog y musica'}
+    title={'Venta de guitarras'}
+    description={'Venta de guitarras, blog y musica'}
     >
-        <h1>Store</h1>
-        <Link href="/">Inicio</Link>
+        <main className='contenedor'>
+          <h1 className='heading'> Nuestra colecciòn</h1>
+          <div className={styles.grid}>
+            {
+              guitars?.length > 0 
+                ? guitars.map(guitar => <Guitar key={guitar.id} guitar ={ guitar }/>)
+                : <p>No hay guitarras para mostrar</p>
+            }
+          </div>
+        </main>
     </Layout>
   )
 }
+
+
+
+// export async function getStaticProps(){
+//   try {
+//     const response = await fetch(`${process.env.API_URL}/guitars?populate=image`);
+//     if (!response.ok) {
+//       throw new Error(`HTTP error! Status: ${response.status}`);
+//     }
+//     const {data} = await response.json();
+//     return {
+//       props: { data },
+//     };
+//   } catch (error) {
+//     console.error('Fetch failed:', error.message);
+//     return {
+//       props: { error: 'Fetch failed' },
+//     };
+//   }
+// }
+
+export async function getServerSideProps(){
+  const response = await fetch(`${process.env.API_URL}/guitars?populate=image`);
+  const { data } = await response.json();
+
+  return {
+    props: {
+      guitars : data
+    }
+  }
+}
+
